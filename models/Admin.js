06d8b5302c34c1100a85fe36459351925c7bfbfc
Harsh <singhharshwardhan124@gmail.com>
2025-12-14 00:16:56 +0530

    xyxy

--- models/Admin.js
+++ models/Admin.js
@@ -6,14 +6,14 @@ const adminSchema = new mongoose.Schema({
   password: { type: String, required: true }
 });
 
-// Password hashing
+// HASH PASSWORD
 adminSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
   this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 
-// Compare password
+// COMPARE PASSWORD
 adminSchema.methods.comparePassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
