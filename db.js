f6a4e4dc462a316cf77f6102f376b42eb9c698c4
Harsh <singhharshwardhan124@gmail.com>
2025-12-13 19:01:03 +0530

    111

--- db.js
+++ db.js
@@ -2,15 +2,11 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI not found in environment");
-    }
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
-    await mongoose.connect(process.env.MONGO_URI);
-
-    console.log("🔥 MongoDB Atlas Connected Successfully");
-  } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
+    console.log(`🔥 MongoDB Connected: ${conn.connection.name}`);
+  } catch (err) {
+    console.error("❌ MongoDB Connection Error:", err.message);
     process.exit(1);
   }
 };
